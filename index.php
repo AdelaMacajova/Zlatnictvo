ad280f784d35a64faa5deab4ebdd1d2c3bdb9b58
Adela Máčajová <adela.macajova3@gmail.com>
2025-03-11 10:10:37 +0100

    verzia1

---
+++ index.php
@@ -0,0 +1,311 @@
+    <?php
+        include('partials/header.php');
+    ?>
+
+    <br>
+    <div class="banner"></div>
+    <br>
+    <hr>
+    <div class="content-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3">
+                    <div class="product-item-1">
+                        <div class="product-thumb">
+                            <img src="images/produkty_zlate/earrings-emerald.jpg" alt="Product Title">
+                        </div> <!-- /.product-thumb -->
+                        <div class="product-content">
+                            <h5><a href="#">New collection with emerald</a></h5>
+                            <span class="tagline">LUXGOLD</span>
+                            <span class="price">250€</span>
+                            <p>Doloremque quo possimus quas necessitatibus blanditiis excepturi. Commodi, sunt asperiores tenetur deleniti labore!</p>
+                        </div> <!-- /.product-content -->
+                    </div> <!-- /.product-item -->
+                </div> <!-- /.col-md-3 -->
+                <div class="col-md-5">
+                    <div class="product-holder">
+                        <div class="product-item-2">
+                            <div class="product-thumb">
+                                <img src="images/produkty_zlate/rings.jpg" alt="Product Title">
+                            </div> <!-- /.product-thumb -->
+                            <div class="product-content overlay">
+                                <h5><a href="#">Gold rings</a></h5>
+                                <span class="tagline">LUXGOLD</span>
+                                <span class="price">250€</span>
+                                <p>Doloremque quo possimus quas necessitatibus blanditiis excepturi. Commodi, sunt tenetur deleniti labore!</p>
+                            </div> <!-- /.product-content -->
+                        </div> <!-- /.product-item-2 -->
+                        <div class="product-item-2">
+                            <div class="product-thumb">
+                                <img src="images/produkty_zlate/earrings-glass.jpg" alt="Product Title">
+                            </div> <!-- /.product-thumb -->
+                            <div class="product-content overlay">
+                                <h5><a href="#">Gold rings - Greece edition</a></h5>
+                                <span class="tagline">LUXGOLD</span>
+                                <span class="price">300€</span>
+                                <p>Doloremque quo possimus quas necessitatibus blanditiis excepturi. Commodi, sunt tenetur deleniti labore!</p>
+                            </div> <!-- /.product-content -->
+                        </div> <!-- /.product-item-2 -->
+                        <div class="clearfix"></div>
+                    </div> <!-- /.product-holder -->
+                </div> <!-- /.col-md-5 -->
+                <div class="col-md-4">
+                    <div class="product-item-3">
+                        <div class="product-thumb">
+                            <img src="images/produkty_zlate/prstene-uvod.jpg" alt="">
+                        </div> <!-- /.product-thumb -->
+                        <div class="product-content">
+                            <div class="row">
+                                <div class="col-md-6 col-sm-6">
+                                    <h5><a href="#">Gemstones in gold rings</a></h5>
+                                    <span class="tagline">LUXGOLD</span>
+                                    <span class="price">399€</span>
+                                </div> <!-- /.col-md-6 -->
+                                <div class="col-md-6 col-sm-6">
+                                    <div class="full-row">
+                                        <label for="cat">Gender:</label>
+                                        <select name="cat" id="cat" class="postform">
+                                            <option value="-1">- Select -</option>
+                                            <option class="level-0" value="49">Female</option>
+                                            <option class="level-0" value="56">Male</option>
+                                        </select>
+                                    </div>
+                                    <div class="full-row">
+                                        <label for="cat1">Size:</label>
+                                        <select name="cat1" id="cat1" class="postform">
+                                            <option value="-1">- Select -</option>
+                                            <option class="level-0" value="49">Small</option>
+                                            <option class="level-0" value="49">Medium</option>
+                                            <option class="level-0" value="56">Large</option>
+                                            <option class="level-0" value="56">X-Large</option>
+                                        </select>
+                                    </div>
+                                    <div class="full-row">
+                                        <label for="cat2">Color:</label>
+                                        <select name="cat2" id="cat2" class="postform">
+                                            <option value="-1">- Select -</option>
+                                            <option class="level-0" value="2">Blue</option>
+                                            <option class="level-0" value="3">Red</option>
+                                            <option class="level-0" value="1">Pink</option>
+                                            <option class="level-0" value="4">Black</option>
+                                            <option class="level-0" value="4">Wlack</option>
+                                        </select>
+                                    </div>
+                                </div> <!-- /.col-md-6 -->
+                                <div class="col-md-12 col-sm-12">
+                                    <div class="button-holder">
+                                        <a href="#" class="red-btn"><i class="fa fa-angle-down"></i></a>
+                                    </div> <!-- /.button-holder -->
+                                </div> <!-- /.col-md-12 -->
+                            </div> <!-- /.row -->
+                        </div> <!-- /.product-content -->
+                    </div> <!-- /.product-item-3 -->
+                </div> <!-- /.col-md-4 -->
+            </div> <!-- /.row -->
+            <div class="row">
+                <div class="col-md-4 col-sm-6 col-xs-12">
+                    <div class="product-item-4">
+                        <div class="product-thumb">
+                            <img src="images/produkty_zlate/outfit with gold.jpg" alt="Product Title">
+                        </div> <!-- /.product-thumb -->
+                        <div class="product-content overlay">
+                            <h5><a href="#">Name Of Shirt</a></h5>
+                            <span class="tagline">Partner Name</span>
+                            <span class="price">$60.00</span>
+                            <p>Doloremque quo possimus quas necessitatibus.</p>
+                        </div> <!-- /.product-content -->
+                    </div> <!-- /.product-item-4 -->
+                </div> <!-- /.col-md-4 -->
+                <div class="col-md-4 col-sm-6 col-xs-12">
+                    <div class="product-item-4">
+                        <div class="product-thumb">
+                            <img src="images/produkty_zlate/bracelets.jpg" alt="Product Title">
+                        </div> <!-- /.product-thumb -->
+                        <div class="product-content overlay">
+                            <h5><a href="#">Name Of Shirt</a></h5>
+                            <span class="tagline">Partner Name</span>
+                            <span class="price">$30.00</span>
+                            <p>Doloremque quo possimus quas necessitatibus.</p>
+                        </div> <!-- /.product-content -->
+                    </div> <!-- /.product-item-4 -->
+                </div> <!-- /.col-md-4 -->
+                <div class="col-md-4 col-sm-12 col-xs-12">
+                    <div class="ads-placemenet">
+                        <img src="images/produkty_zlate/pearls.jpg" alt="">
+                    </div> <!-- /.ads-placement -->
+                </div> <!-- /.col-md-4 -->
+            </div> <!-- /.row -->
+        </div> <!-- /.container -->
+    </div> <!-- /.content-section -->
+
+    <div class="content-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 section-title">
+                    <h2>New Products</h2>
+                </div> <!-- /.section -->
+            </div> <!-- /.row -->
+            <div class="row">
+                <div class="col-md-3 col-sm-6">
+                    <div class="product-item">
+                        <div class="product-thumb">
+                            <img src="images/produkty_zlate/emerald.png" alt="">
+                        </div> <!-- /.product-thum -->
+                        <div class="product-content">
+                            <h5><a href="#">Ring with emerald</a></h5>
+                            <span class="price">280€</span>
+                        </div> <!-- /.product-content -->
+                    </div> <!-- /.product-item -->
+                </div> <!-- /.col-md-3 -->
+                <div class="col-md-3 col-sm-6">
+                    <div class="product-item">
+                        <div class="product-thumb">
+                            <img src="images/produkty_zlate/ruby.png" alt="">
+                        </div> <!-- /.product-thum -->
+                        <div class="product-content">
+                            <h5><a href="#">Ring with ruby</a></h5>
+                            <span class="price">270€</span>
+                        </div> <!-- /.product-content -->
+                    </div> <!-- /.product-item -->
+                </div> <!-- /.col-md-3 -->
+                <div class="col-md-3 col-sm-6">
+                    <div class="product-item">
+                        <div class="product-thumb">
+                            <img src="images/produkty_zlate/sapphire2.png" alt="">
+                        </div> <!-- /.product-thum -->
+                        <div class="product-content">
+                            <h5><a href="#">Ring with sapphire</a></h5>
+                            <span class="price">240€</span>
+                        </div> <!-- /.product-content -->
+                    </div> <!-- /.product-item -->
+                </div> <!-- /.col-md-3 -->
+                <div class="col-md-3 col-sm-6">
+                    <div class="product-item">
+                        <div class="product-thumb">
+                            <img src="images/produkty_zlate/citrine.png" alt="">
+                        </div> <!-- /.product-thum -->
+                        <div class="product-content">
+                            <h5><a href="#">Ring with citrine</a></h5>
+                            <span class="price">250€</span>
+                        </div> <!-- /.product-content -->
+                    </div> <!-- /.product-item -->
+                </div> <!-- /.col-md-3 -->
+            </div> <!-- /.row -->
+        </div> <!-- /.container -->
+    </div> <!-- /.content-section -->
+
+    <div class="content-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 section-title">
+                    <h2>Vote For Future Products</h2>
+                </div> <!-- /.section -->
+            </div> <!-- /.row -->
+            <div class="row">
+                <div class="col-md-3 col-sm-6">
+                    <div class="product-item-vote">
+                        <div class="product-thumb">
+                            <img src="images/produkty_zlate/new_product1.jpg" alt="">
+                        </div> <!-- /.product-thum -->
+                        <div class="product-content">
+                            <h5><a href="#">Aurora</a></h5>
+                            <span class="tagline">By: LUXGOLD</span>
+                            <ul class="progess-bars">
+                                <li>
+                                    <div class="progress">
+                                        <div class="progress-bar" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 40%;"></div>
+                                        <span>4<i class="fa fa-heart"></i></span>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="progress">
+                                        <div class="progress-bar comments" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;"></div>
+                                        <span class="comments">6<i class="fa fa-heart"></i></span>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div> <!-- /.product-content -->
+                    </div> <!-- /.product-item-vote -->
+                </div> <!-- /.col-md-3 -->
+                <div class="col-md-3 col-sm-6">
+                    <div class="product-item-vote">
+                        <div class="product-thumb">
+                            <img src="images/produkty_zlate/new_product2 (2).jpg" alt="">
+                        </div> <!-- /.product-thum -->
+                        <div class="product-content">
+                            <h5><a href="#">Python</a></h5>
+                            <span class="tagline">By: LUXGOLD</span>
+                            <ul class="progess-bars">
+                                <li>
+                                    <div class="progress">
+                                        <div class="progress-bar" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 40%;"></div>
+                                        <span>4<i class="fa fa-heart"></i></span>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="progress">
+                                        <div class="progress-bar comments" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;"></div>
+                                        <span class="comments">6<i class="fa fa-heart"></i></span>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div> <!-- /.product-content -->
+                    </div> <!-- /.product-item-vote -->
+                </div> <!-- /.col-md-3 -->
+                <div class="col-md-3 col-sm-6">
+                    <div class="product-item-vote">
+                        <div class="product-thumb">
+                            <img src="images/produkty_zlate/new_product3.jpg" alt="">
+                        </div> <!-- /.product-thum -->
+                        <div class="product-content">
+                            <h5><a href="#">Lunar Glow</a></h5>
+                            <span class="tagline">By: LUXGOLD</span>
+                            <ul class="progess-bars">
+                                <li>
+                                    <div class="progress">
+                                        <div class="progress-bar" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 40%;"></div>
+                                        <span>4<i class="fa fa-heart"></i></span>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="progress">
+                                        <div class="progress-bar comments" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;"></div>
+                                        <span class="comments">6<i class="fa fa-heart"></i></span>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div> <!-- /.product-content -->
+                    </div> <!-- /.product-item-vote -->
+                </div> <!-- /.col-md-3 -->
+                <div class="col-md-3 col-sm-6">
+                    <div class="product-item-vote">
+                        <div class="product-thumb">
+                            <img src="images/produkty_zlate/new_product4.jpg" alt="">
+                        </div> <!-- /.product-thum -->
+                        <div class="product-content">
+                            <h5><a href="#">Helios</a></h5>
+                            <span class="tagline">By: LUXGOLD</span>
+                            <ul class="progess-bars">
+                                <li>
+                                    <div class="progress">
+                                        <div class="progress-bar" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 40%;"></div>
+                                        <span>4<i class="fa fa-heart"></i></span>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="progress">
+                                        <div class="progress-bar comments" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;"></div>
+                                        <span class="comments">6<i class="fa fa-heart"></i></span>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div> <!-- /.product-content -->
+                    </div> <!-- /.product-item-vote -->
+                </div> <!-- /.col-md-3 -->
+            </div> <!-- /.row -->
+        </div> <!-- /.container -->
+    </div> <!-- /.content-section -->
+
+   <?php
+   include('partials/footer.php')
+   ?>
